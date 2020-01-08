26ce06d1e8d5410178e175cb83cbb8673065142b
Michael Kovaliov <michaelkovaliov97@gmail.com>
2020-01-08 22:04:55 +0200

    Refactored the code so now it works

--- retrain_imagenet_classifier/sound_to_image.py
+++ retrain_imagenet_classifier/sound_to_image.py
@@ -6,10 +6,14 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa.display
-import time
 
 count = -1
-with open('metadata/UrbanSound8K.csv') as csvfile:
+URBAN_SOUND8K_PATH = os.path.join(os.path.pardir, 'UrbanSound8K')
+URBAN_SOUND8K_CSV_PATH = os.path.join(URBAN_SOUND8K_PATH, os.path.join('metadata', 'UrbanSound8K.csv'))
+AUDIO_PATH = os.path.join(URBAN_SOUND8K_PATH, 'audio')
+
+
+with open(URBAN_SOUND8K_CSV_PATH) as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
         count += 1
@@ -17,17 +21,17 @@ with open('metadata/UrbanSound8K.csv') as csvfile:
         if count == 0:
             continue
 
-        print count
+        print(count)
         if not os.path.exists('spectrograms/' + row[7]):
             os.makedirs('spectrograms/' + row[7])
 
-        y, sr = librosa.load("audio/fold" + str(row[5]) + "/" + str(row[0]))
+        y, sr = librosa.load(os.path.join(AUDIO_PATH, 'fold') + str(row[5]) + os.path.sep + str(row[0]))
 
         # Let's make and display a mel-scaled power (energy-squared) spectrogram
         S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
 
         # Convert to log scale (dB). We'll use the peak power as reference.
-        log_S = librosa.logamplitude(S, ref_power=np.max)
+        log_S = librosa.amplitude_to_db(S, ref=np.max)
 
         # Make a new figure
         fig = plt.figure(figsize=(12, 4))
@@ -45,4 +49,4 @@ with open('metadata/UrbanSound8K.csv') as csvfile:
         plt.savefig('spectrograms/' + row[7] + '/' + row[0] + '.png')
         plt.close()
 
-        print count
+        print(count)
