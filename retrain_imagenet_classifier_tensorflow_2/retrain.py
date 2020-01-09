299ba9f3b9fab51f591ef6b68efe7439c8ae4c42
Michael Kovaliov <michaelkovaliov97@gmail.com>
2020-01-09 14:33:50 +0200

    Added a first working version of a retrain.py with tensorflow-2.

---
+++ retrain_imagenet_classifier_tensorflow_2/retrain.py
@@ -0,0 +1,130 @@
+import matplotlib.pylab as plt
+import tensorflow as tf
+
+import tensorflow_hub as hub
+from tensorflow.keras import layers
+import time
+import numpy as np
+import sys
+import PIL.Image as Image
+
+
+class CollectBatchStats(tf.keras.callbacks.Callback):
+    def __init__(self):
+        self.batch_losses = []
+        self.batch_acc = []
+
+    def on_train_batch_end(self, batch, logs=None):
+        self.batch_losses.append(logs['loss'])
+        self.batch_acc.append(logs['acc'])
+        self.model.reset_metrics()
+
+
+def fix_gpu():
+    '''
+    I don't fully understand this yet but these lines fix an error in CUDNN
+    '''
+
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+
+
+IMAGE_SHAPE = (224, 224)
+DATA_ROOT = r'D:\Working\SoundClassifier\retrain_imagenet_classifier\spectrograms'
+EXPORT_PATH = 'D:\\tmp\\saved_models\\spectogram'
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: {} load/predict".format(sys.argv[0]))
+    should_load = sys.argv[1] == 'load'
+    fix_gpu()
+
+    # Obtain data to memory.
+    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
+    image_data = image_generator.flow_from_directory(str(DATA_ROOT), target_size=IMAGE_SHAPE)
+    image_batch, label_batch = image_data[0]
+
+    # Now we want to retrain:
+    feature_extractor_url = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2'
+    feature_extractor_layer = hub.KerasLayer(feature_extractor_url,
+                                             input_shape=(224, 224, 3))
+    feature_batch = feature_extractor_layer(image_batch)
+
+    feature_extractor_layer.trainable = False
+
+    # Add the final layer to the ready model to train.
+    model = tf.keras.Sequential([feature_extractor_layer, layers.Dense(image_data.num_classes,
+                                                                       activation='softmax')])
+    print(model.summary())
+    if not should_load:
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss='categorical_crossentropy',
+            metrics=['acc'])
+
+        steps_per_epoch = np.ceil(image_data.samples / image_data.batch_size)
+
+        batch_stats_callback = CollectBatchStats()
+
+        history = model.fit_generator(image_data, epochs=2,
+                                      steps_per_epoch=steps_per_epoch,
+                                      callbacks=[batch_stats_callback])
+
+        plt.figure()
+        plt.ylabel('Loss')
+        plt.xlabel('Training Steps')
+        plt.ylim([0, 2])
+        plt.plot(batch_stats_callback.batch_losses)
+
+        plt.figure()
+        plt.ylabel('Accuracy')
+        plt.xlabel('Training Steps')
+        plt.ylim([0, 1])
+        plt.plot(batch_stats_callback.batch_acc)
+
+        plt.show()
+
+        class_names = sorted(image_data.class_indices.items(), key=lambda pair: pair[1])
+        class_names = np.array([key.title() for key, value in class_names])
+        print(class_names)
+
+        predicted_batch = model.predict(image_batch)
+        predicted_id = np.argmax(predicted_batch, axis=-1)
+        predicted_label_batch = class_names[predicted_id]
+
+        label_id = np.argmax(label_batch, axis=-1)
+        plt.figure(figsize=(10, 9))
+        plt.subplots_adjust(hspace=0.5)
+        for n in range(30):
+            plt.subplot(6, 5, n + 1)
+            plt.imshow(image_batch[n])
+            color = 'green' if predicted_id[n] == label_id[n] else 'red'
+            plt.title(predicted_label_batch[n].title(), color=color)
+            plt.axis('off')
+        _ = plt.suptitle('Model predictions (green: correct, red: incorrect)')
+        plt.show()
+
+        model.save(EXPORT_PATH, save_format='tf')
+    else:
+        model = tf.keras.models.load_model(EXPORT_PATH)
+
+        class_names = sorted(image_data.class_indices.items(), key=lambda pair: pair[1])
+        class_names = np.array([key.title() for key, value in class_names])
+        print(class_names)
+        predicted_batch = model.predict(image_batch)
+        predicted_id = np.argmax(predicted_batch, axis=-1)
+        predicted_label_batch = class_names[predicted_id]
+
+        label_id = np.argmax(label_batch, axis=-1)
+        plt.figure(figsize=(10, 9))
+        plt.subplots_adjust(hspace=0.5)
+        for n in range(30):
+            plt.subplot(6, 5, n + 1)
+            plt.imshow(image_batch[n])
+            color = 'green' if predicted_id[n] == label_id[n] else 'red'
+            plt.title(predicted_label_batch[n].title() + ", " + class_names[label_id[n]], color=color)
+            plt.axis('off')
+        _ = plt.suptitle('Model predictions (green: correct, red: incorrect)')
+        plt.show()
+    print(EXPORT_PATH)
